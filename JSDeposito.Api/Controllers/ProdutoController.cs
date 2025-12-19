922ee0b8dd4751db2ab19bd7806d9d18b2ce8407
Leandro D'Jesus <crazyg4ng@gmail.com>
2025-12-18 21:46:39 -0300

    chore(API + Injeção): Controller /baixar + injeção de dependencia do repositorio e service

---
+++ JSDeposito.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,23 @@
+﻿using JSDeposito.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSDeposito.Api.Controllers;
+
+[ApiController]
+[Route("api/produtos")]
+public class ProdutoController : ControllerBase
+{
+    private readonly EstoqueService _estoqueService;
+
+    public ProdutoController(EstoqueService estoqueService)
+    {
+        _estoqueService = estoqueService;
+    }
+
+    [HttpPost("{id}/baixar")]
+    public IActionResult Baixar(int id, int quantidade)
+    {
+        _estoqueService.Baixar(id, quantidade);
+        return Ok();
+    }
+}
--- JSDeposito.Api/Program.cs
+++ JSDeposito.Api/Program.cs
@@ -1,3 +1,8 @@
+using JSDeposito.Api.Data;
+using JSDeposito.Core.Interfaces;
+using JSDeposito.Core.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("Default"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("Default"))
+    );
+});
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<EstoqueService>();
 
 var app = builder.Build();
 
